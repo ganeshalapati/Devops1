6e1e017dcd5881e1816e7b6dd79c071321d7a16b
ganeshalapati <82505098+ganeshalapati@users.noreply.github.com>
2026-02-26 02:43:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const http = require('http');
 http.createServer((req,res)=>{
 res.write("CI/CD Pipeline Working 🚀");
 res.write("CI/CD all Working 🚀");
+res.write("CI/CD my project Working 🚀")
 res.end();
 }).listen(3000);
